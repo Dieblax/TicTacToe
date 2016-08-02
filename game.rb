a798a3485565bc3c62f26cd210b70fd62523683b
Diego Vurgait <diegovurgait@hotmail.com>
2016-08-02 18:34:48 +0200

    Almost done with Human v Human, win bug tho

--- game.rb
+++ game.rb
@@ -1,4 +1,5 @@
-class Game
+class Game 
+
 	def initialize
 		@cases = 
 						[
@@ -6,7 +7,47 @@ class Game
 							[" ", " ", " "],
 							[" ", " ", " "]
 						]
+		@pos_hash = Hash.new { |h, k| h[k] = [] }
+		display
 	end
+
+	def play(player, sym)
+		puts "Player #{player}, it's your turn."
+		begin
+			puts "Where do you want to play? (e.g. A1)"
+			pos = gets.strip
+			pos = pos.split("")
+			y = pos[0].downcase.ord - 97
+			x = pos[1].to_i - 1
+			if @cases[x][y] != " "
+				raise "Illegal move"
+			end
+			@cases[x][y] = sym.to_s.downcase
+		rescue 
+			puts "It's an illegal move!\n"
+			retry
+		end
+		display
+	end
+
+	def over?
+		state_update
+
+		if win?(:x)
+			puts "Player 1 wins!\n"
+		elsif win?(:o)
+			puts "Player 2 wins!\n"
+		elsif draw?
+			puts "It's a draw...\n"
+		else
+			return false
+		end
+
+		return true
+
+	end
+
+	private
 
 	def display
 		puts `clear`
@@ -16,5 +57,77 @@ class Game
 			puts "  " + "-" * 11 unless i == 2
 		end
 	end
-	
+
+	# updates @pos_hash which contains all positions occupied by both :x and :o
+	def state_update
+
+		@cases.each_with_index do |row, i|
+			row.each_with_index do |pos, j|
+				@pos_hash[pos.to_sym].push([i, j])
+			end
+		end
+
+	end
+
+	def win?(sym)
+		if vertical_win?(@pos_hash[sym]) || horizontal_win?(@pos_hash[sym]) || diagonal_win?(@pos_hash[sym])
+			return true
+		end
+		return false
+	end
+
+	def vertical_win?(arr)
+		counter = Hash.new { |h, k| h[k] = 0 }
+		arr.each do |x, y|
+			p counter # to delete
+			counter[y] += 1
+		end
+		win = false
+		(0..2).each do |y|
+			if counter[y] == 3
+				puts "Vertical win, hash : #{counter}" # to delete
+				win = true 
+			end
+		end
+		return win
+	end
+
+	def horizontal_win?(arr)
+		counter = Hash.new { |h, k| h[k] = 0 }
+		arr.each do |x, y|
+			p counter # to delete
+			counter[x] += 1
+		end
+		win = false
+		(0..2).each do |x|
+			if counter[x] == 3
+				puts "Horizontal win, hash : #{counter}" # to delete
+				win = true 
+			end
+		end
+		return win
+	end
+
+	def diagonal_win?(arr)
+		corners = {top_left: [0, 0], top_right: [0, 2], bottom_left: [2, 0], bottom_right: [2, 2]}
+		left_right = [corners[:top_left], corners[:bottom_right]]
+		right_left = [corners[:top_right], corners[:bottom_left]]
+		if arr.include?([1, 1])
+			if left_right.all? {|corner| arr.include?(corner)} || right_left.all? {|corner| arr.include?(corner)}
+				puts "Diagonal win, hash : #{counter}" # to delete
+				return true
+			end
+		else
+			return false
+		end
+	end
+
+	def draw?
+		if @cases.any? {|row| row.include?(" ")}
+			return false
+		else
+			return true
+		end
+	end
+
 end
--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -1,5 +1,18 @@
 require_relative 'game'
 require_relative 'player'
 
-game = Game.new
-game.display
+again = true
+while again
+
+	game = Game.new
+	turn = 0
+	until game.over?
+		turn = (turn % 2)
+		p turn
+		(turn + 1).odd? ? sym = :x : sym = :o
+		game.play(turn + 1, sym)
+		turn += 1
+	end
+
+	again = false
+end
